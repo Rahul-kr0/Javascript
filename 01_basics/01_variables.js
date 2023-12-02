4376f6bee75f2cf535fed50ceec72e6f4c1684a2
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-02 05:29:31 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,22 @@
-const accountId = 14453;
-let accountEmail = "rahul@gmail.com"
-var accountPassword = "1234"
-accountCity = "Ranchi"
+const accountId = 14453;               // const values can't be changed
+let accountEmail = "rahul@gmail.com"; // let values can be changed 
+var accountPassword = "1234";         // var values can be changed
+accountCity = "Ranchi";
+let accountState; // undefined variable
 
 // accountId = 2 // not allowed
 
+accountEmail = "htc@gmail.com"; 
+accountPassword = "456";
+accountCity = "bihar";
+
 console.log(accountId);
+
+/*
+
+prefer not to use var
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); 
+// table is used to represent more than one output in a form of table
