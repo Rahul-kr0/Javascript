14f554b85bf4f5978016bad13c857ed6e60e222b
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-03 06:28:55 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+
+// number
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
