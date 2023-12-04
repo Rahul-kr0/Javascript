d7f26c0d3d4aba050ae94cd3fbeae94235118dae
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-04 05:14:14 +0000

    conversion of data type

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,17 @@
-let score = "33"
+let score = "33a"
 
 console.log(typeof (score));
 console.log(score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// true => 1; false => 0
+// "" => false 
+//"Rahul" => true
+
