67f74d565b3f3ca8fc07e003e27b37cc8681d36f
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-05 06:22:23 +0000

    DataTypes

---
+++ 01_basics/05_dataTypes.js
@@ -0,0 +1,24 @@
+
+// Primitive DataType
+
+// 7 DataTypes: Number, String, Boolean, null, undefined, Symbol, BigInt
+
+const value = 100;   // Number
+const val = 10.9;    // Number
+const str = "Rahul"; // String
+const bool = true;   // Boolean
+const nul = null;    // null
+let undef;           // undefined
+
+const sybl = Symbol("9234") // Symbol
+const syl  = Symbol("9234")
+console.log(sybl === syl); // output will be false
+// Because symbol represents unique charater or number ever they are same
+
+const bigNumber = 987654323567987654234n; // BigInt
+
+
+
+// Reference or Non primitive DataType
+
+// Array, Objects, Functions
