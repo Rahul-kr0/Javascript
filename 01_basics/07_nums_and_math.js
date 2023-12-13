95fd22d9b066a9e7529c30de7b0311c2e8ad3dfa
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-13 05:20:47 +0000

    numbers

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,13 @@
+const score = 400;
+
+const balance = new Number(200);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const value = 9432.7890;
+console.log(value.toPrecision(5));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString());
