405008e696919746736f2bfaa0d3d1a4c022a4de
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-15 15:08:30 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,16 @@
+const myarr = [0, 1, 2, 3, 4, 5];
+
+const myarr2 = new Array(1, 2, 3, 4, 5);
+console.log(myarr[0]);
+
+// Array methods
+
+myarr.push(9); // To add value in last
+myarr.pop();   // To delete last value
+myarr.unshift(8); // To add value in first
+myarr.shift();  // To remove value from first
+console.log(myarr.includes(9)); // To find the vale exists in a array or not
+console.log(myarr.indexOf(4)); // To find the index of an element in an array
+
+const newarr = myarr.join(); // It changes the array in string
+console.log(typeof newarr);
