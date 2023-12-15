68f5308222ef4acc1a0bb2a581db76077b1b62b5
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-15 13:44:43 +0000

    dates and time

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -10,3 +10,15 @@ console.log(mydate.toLocaleTimeString()); // 2:39:51 AM
 console.log(mydate.toString());      // Fri Dec 15 2023 02:40:36 GMT+0000 (Coordinated Universal Time)
 console.log(mydate.toTimeString());  // 02:41:48 GMT+0000 (Coordinated Universal Time)
 console.log(mydate.toUTCString());   // Fri, 15 Dec 2023 02:42:34 GMT
+
+let myCreatedDate = new Date("01-14-2023");
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getDay()+1);
