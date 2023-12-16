ed8d0b55ac45121e779f8bac04e0b8c79ea4ed1a
Rahul Kumar <rahulkr76459@gmail.com>
2023-12-16 15:54:18 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -14,3 +14,8 @@ console.log(myarr.indexOf(4)); // To find the index of an element in an array
 
 const newarr = myarr.join(); // It changes the array in string
 console.log(typeof newarr);
+
+console.log(myarr.slice(1,3)); // To get the range of the value
+
+console.log(myarr.splice(1,3)); // To get the exact value from an array
+console.log(myarr);
